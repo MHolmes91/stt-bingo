b2e6f9aa878c5cf104f658774a74fa49990190d4
Mark Holmes <contact@markholm.es>
2019-03-07 00:58:35 -0600

    Updates

---
+++ precache-manifest.3666528058825f66b645d654ad5b4add.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca38ab4113d12bd8de7",
+    "url": "/stt-bingo/static/css/main.4f1fabd1.chunk.css"
+  },
+  {
+    "revision": "2ca38ab4113d12bd8de7",
+    "url": "/stt-bingo/static/js/main.18546663.chunk.js"
+  },
+  {
+    "revision": "ceee0815cffc2d6f63f6",
+    "url": "/stt-bingo/static/js/runtime~main.60310ba1.js"
+  },
+  {
+    "revision": "8d3ead78e11bce60c4d0",
+    "url": "/stt-bingo/static/js/2.5df5b2b8.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stt-bingo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "51356d67d0bfba9543a8c397b776b403",
+    "url": "/stt-bingo/index.html"
+  }
+];
