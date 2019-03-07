3465ebd806e91ecbbec428378eab620e05cccc14
Mark Holmes <contact@markholm.es>
2019-03-07 00:07:28 -0600

    Creepily continuous speech to text

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import injectJSS from 'react-jss'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const styles = {
+  app: {
+    textAlign: 'center'
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '20vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white'
+  },
+  appLogo: {
+    animation: 'App-logo-spin infinite 20s linear',
+    height: '10vmin',
+    pointerEvents: 'none',
+  },
+  appLink: {
+    color: '#61dafb'
+  },
+  '@keyframes App-logo-spin': {
+    from: {
+      transform: 'rotate(0deg)'
+    },
+    to: {
+      transform: 'rotate(360deg)'
+    }
   }
 }
 
-export default App;
+const App = ({ classes }) => {
+  const [isRecognizing, setIsRecognizing] = useState(false)
+  const [words, setWords] = useState('')
+  const [speechRecognition, setSpeechRecognition] = useState(null)
+
+  useEffect(() => {
+    if (!('webkitSpeechRecognition' in window)) {
+      console.log('no speech recognition')
+    } else {
+      const recognition = new window.webkitSpeechRecognition();
+      recognition.continuous = true
+      recognition.interimResults = false
+      recognition.lang = 'en-US'
+    
+      recognition.onstart = () => setIsRecognizing(true)
+      recognition.onresult = event => {
+        console.log(event)
+        let resultWords = ''
+        for(let i = 0; i < event.results.length; i++){
+          const result = event.results[i]
+          for(let n = 0; n < result.length; n++){
+            const resultPart = result[n]
+            resultWords += resultPart.transcript
+          }
+        }
+        console.log(resultWords)
+        setWords(words + resultWords + ' ')
+      }
+      recognition.onerror = event => {
+        console.log('error', event)
+        if(event.error === 'no-speech'){
+          setTimeout(() => {
+            recognition.start() //Keep it coming
+          })
+          
+        }
+        //setWords('error')
+      }
+      recognition.onend = () => setIsRecognizing(false)
+
+      recognition.start();
+
+      setSpeechRecognition(recognition)
+    }
+  }, [])
+
+  const toggleRecognition = () => speechRecognition
+    && (isRecognizing
+      ? speechRecognition.stop()
+      : speechRecognition.start()
+    )
+
+  return (
+    <div className={classes.app}>
+      <header className={classes.appHeader}>
+        <img src={logo} className={classes.appLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className={classes.appLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <main>
+        <div>
+          <button onClick={toggleRecognition}>{isRecognizing ? 'Stop' : 'Start'}</button>
+        </div>
+        <div>{words}</div>
+      </main>
+    </div>
+  )
+}
+
+export default injectJSS(styles)(App);
